498be67aa4c97b482cee9f6cd3b6a8cb24bba982
AzMa-sys <jhono@bk.ru>
2025-11-09 01:34:46 +0300

    Update settings.py

--- carprotect/carprotect/settings.py
+++ carprotect/carprotect/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = environ.get("DJANGO_SECRET_KEY") #'django-insecure-w7mlnfjwre=&(9-u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = environ.get("DJANGO_ALLOWED_HOSTS").split(" ") #, ['127.0.0.1', ] ]
+ALLOWED_HOSTS = environ.get("DJANGO_ALLOWED_HOSTS").split(" ") # '127.0.0.1'
 
 
 # Application definition
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': environ.get('DATABASE_NAME'),
         'USER': environ.get('DATABASE_USERNAME'),
         'PASSWORD': environ.get('DATABASE_PASSWORD'),
-        'HOST': 'postgres',
+        'HOST': environ.get('DATABASE_HOST'), #'postgres',
         'PORT': environ.get('DATABASE_PORT'),
     }
     # 'default': {
